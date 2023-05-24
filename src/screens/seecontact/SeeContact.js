41d195979cb92f4d4259be2f082b0072f471f7ac
BugVogel <bugvogel@gmail.com>
2023-05-24 11:48:25 -0300

    Adujust for button absolute event

--- src/screens/seecontact/SeeContact.js
+++ src/screens/seecontact/SeeContact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {
   ContactBannerWrapper,
@@ -7,19 +7,39 @@ import {
 } from './styles';
 import ContactBanner from '../../components/contactbanner/ContactBanner';
 import InfoContact from '../../components/infocontact/InfoContact';
+import AbsoluteButton from '../../components/absolutebutton/AbsoluteButton';
 
 const SeeContact = props => {
-  //   props.navigation.setOptions({title: props.route.params.name});
+  const [showAbsoluteButtonText, setShowAbsoluteButtonText] = useState(false);
+  const [currentOffset, setCurrentOffset] = useState(0);
   const contact = props.route.params;
   return (
-    <SeeContactsContainer>
-      <ContactBannerWrapper>
-        <ContactBanner name={contact.name} number={230219} />
-      </ContactBannerWrapper>
-      <InfoContactWrapper>
-        <InfoContact contact={contact} />
-      </InfoContactWrapper>
-    </SeeContactsContainer>
+    <>
+      <SeeContactsContainer
+        onScroll={event => {
+          if (event.nativeEvent.contentOffset.y > currentOffset) {
+            setShowAbsoluteButtonText(true);
+          } else {
+            setShowAbsoluteButtonText(false);
+          }
+          setCurrentOffset(event.nativeEvent.contentOffset.y);
+        }}>
+        <ContactBannerWrapper>
+          <ContactBanner name={contact.name} number={230219} />
+        </ContactBannerWrapper>
+        <InfoContactWrapper>
+          <InfoContact contact={contact} />
+        </InfoContactWrapper>
+      </SeeContactsContainer>
+      <AbsoluteButton
+        iconName={''}
+        label={'Editar contato'}
+        showText={showAbsoluteButtonText}
+        onPress={() =>
+          props.navigation.navigate('EditCreateContact', {...contact})
+        }
+      />
+    </>
   );
 };
 export default SeeContact;
