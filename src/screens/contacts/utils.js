b7c6d7da449893f3a52ebed9c9a9d5e97c4c1d3d
BugVogel <bugvogel@gmail.com>
2023-05-25 17:26:33 -0300

    Adjust order list contacts

--- src/screens/contacts/utils.js
+++ src/screens/contacts/utils.js
@@ -2,7 +2,7 @@ export const getFirstIndexs = listJSON => {
   let firstLetter = '';
   return listJSON
     .map((contact, index) => {
-      const contactFirstLetter = contact.name.trim()[0];
+      const contactFirstLetter = contact.name.trim()[0].toUpperCase();
       if (firstLetter === '' || contactFirstLetter !== firstLetter) {
         firstLetter = contactFirstLetter;
         return index;
@@ -15,9 +15,9 @@ export const orderListJSON = listJSON => {
   let listOrded = [...listJSON];
 
   listOrded.sort(({name: name1}, {name: name2}) => {
-    if (name1 > name2) {
+    if (name1.toUpperCase() > name2.toUpperCase()) {
       return 1;
-    } else if (name1 < name2) {
+    } else if (name1.toUpperCase() < name2.toUpperCase()) {
       return -1;
     }
     return 0;
